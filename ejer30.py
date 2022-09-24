d8aa6137cf1b2e12ec6d4cc341511b09e42f4e79
Alejo <alejoepucheta89@gmail.com>
2022-09-24 01:37:29 -0300

    version mejorada

--- ejer30.py
+++ ejer30.py
@@ -1,15 +1,28 @@
-palabra1 = input("Ingrese una palabra: ")
-palabra2 = input("Ingrese una palabra: ")
-palabra3 = input("Ingrese una palabra: ")
-palabra4 = input("Ingrese una palabra: ")
-palabra5 = input("Ingrese una palabra: ")
+lista_palabras = []
+for x in range(0,5):
+    palabra = input("Ingrese una palabra: ")
+    lista_palabras.append(palabra)
 
-long_palabra1 = len(palabra1)
-long_palabra2 = len(palabra2)
-long_palabra3 = len(palabra3)
-long_palabra4 = len(palabra4)
-long_palabra5 = len(palabra5)
+contador = 0
+for i in lista_palabras:
+    contador += len(i)
 
-total_cant_letras = long_palabra1 + long_palabra2 + long_palabra3 + long_palabra4 + long_palabra5
+print(f"La cantidad de letras en total es: {contador}")
 
-print(total_cant_letras)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
