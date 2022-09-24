ab92e1cb4862874c477e5fd7c1d7b161a62cb789
Alejo <alejoepucheta89@gmail.com>
2022-09-24 01:04:12 -0300

    prueba

---
+++ ejer30.py
@@ -0,0 +1,15 @@
+palabra1 = input("Ingrese una palabra: ")
+palabra2 = input("Ingrese una palabra: ")
+palabra3 = input("Ingrese una palabra: ")
+palabra4 = input("Ingrese una palabra: ")
+palabra5 = input("Ingrese una palabra: ")
+
+long_palabra1 = len(palabra1)
+long_palabra2 = len(palabra2)
+long_palabra3 = len(palabra3)
+long_palabra4 = len(palabra4)
+long_palabra5 = len(palabra5)
+
+total_cant_letras = long_palabra1 + long_palabra2 + long_palabra3 + long_palabra4 + long_palabra5
+
+print(total_cant_letras)
